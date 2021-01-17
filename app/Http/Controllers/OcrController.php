73252ac842d198a0280f12197bbc2c6b46ce8828
mengfong96 <leemengfong.96@gmail.com>
2021-01-17 22:52:15 +0800

    [modify] modify file name & use try catch with albert

--- app/Http/Controllers/OcrController.php
+++ app/Http/Controllers/OcrController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use thiagoalessio\TesseractOCR\TesseractOCR;
+use thiagoalessio\TesseractOCR\UnsuccessfulCommandException;
 use App\Models\Ocr;
 
 class OcrController extends Controller
@@ -28,23 +29,35 @@ class OcrController extends Controller
         }
 
         if ($request->file('file')) {
+            $originalName = $request->file('file')->getClientOriginalName();
+            $originalExt = $request->file('file')->getClientOriginalExtension();
+            $originalPathName = pathinfo($originalName, PATHINFO_FILENAME);
 
-            $file = $request->file('file')->store('documents', 'public');
+            $originalFileName = $originalPathName . "_" . time() . "." . $originalExt;
+            $file = $request->file('file')->storeAs('public/documents', $originalFileName);
 
             if ($file) {
-                $read = (new TesseractOCR($request->file('file')))->run();
-                if ($read) {
+                try {
+                    $read = (new TesseractOCR($request->file('file')))->run();
+                    if ($read) {
 
-                    //Upload log
-                    Ocr::create([
-                        'file_name' => $file,
-                        'output_text' => $read,
-                    ]);
+                        //Upload log
+                        Ocr::create([
+                            'file_name' => $originalFileName,
+                            'output_text' => $read,
+                        ]);
 
+                        return response()->json([
+                            'status' => 'success',
+                            'message' => 'Text extracted successfully',
+                            'text' => $read
+                        ], 201);
+                    }
+                } catch (UnsuccessfulCommandException $e) {
                     return response()->json([
-                        'message' => 'Text extracted successfully',
-                        'text' => $read
-                    ], 201);
+                        'status' => 'failed',
+                        'message' => 'Error:' . $e,
+                    ], 403);
                 }
             }
         }
